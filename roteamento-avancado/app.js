0a1b708a28ef41ee9c9ebcd4d413323198d4345f
Leonardo Souza <leonardo.souza@luizalabs.com>
2022-08-19 22:02:01 -0300

    aula 02

--- roteamento-avancado/app.js
+++ roteamento-avancado/app.js
@@ -4,10 +4,11 @@ console.log(`=== ${env.APP_NAME} ===`);
 const express = require("express");
 const app = express();
 
+// Parseando Body Request
 app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
 
-// Roteamento (Avançado)
+// Roteamento (Básico)
 // Mapeando Rotas
 // app.method("route", callback);
 
@@ -15,14 +16,7 @@ app.use(express.json());
 // res - Server Response
 // app.get("/books", function(req, res) {});
 
-// Create
-app.post("/books", (req, res) => {
-  console.log(`REQUEST POST`);
-  res.end(`RESPONSE POST!`)
-});
-
-// Read
-app.get("/books/:id", (req, res) => {
+const handler = (req, res) => {
   // Query Params 
   console.log(`QUERY PARAMS`, req.query);
 
@@ -32,21 +26,48 @@ app.get("/books/:id", (req, res) => {
   // Body Request
   console.log(`BODY REQ`, req.body);
 
-  res.end(`RESPONSE GET!`)
-});
+  res.end(`RESPONSE ${req.method}!`)
+};
+
+// Create
+app.post("/books", handler);
+
+// Read
+app.get("/books", handler);
 
 // Update
-app.put("/books", (req, res) => {
-  res.end(`RESPONSE PUT!`)
-});
-
-app.patch("/books", (req, res) => {
-  res.end(`RESPONSE PATCH!`)
-});
+app.put("/books", handler);
+app.patch("/books", handler);
 
 // Delete
-app.delete("/books", (req, res) => {
-  res.end(`RESPONSE DELETE!`)
-});
+app.delete("/books", handler);
+
+
+// Roteamento (Avançado)
+// Caracteres Especiais: ? + * () |
+
+const advancedHandler = (req, res) => {
+  console.log(`ROUTE: ${req.method} ${req.url}`);
+  res.end(`END: ${req.url}`);
+};
+
+// ? Opcional 
+app.get("/pages?", advancedHandler);
+
+// + Repetidor
+app.get("/ses+ion", advancedHandler);
+
+// * Tudo
+app.get("/auth*", advancedHandler);
+
+// () Agrupador
+// | Ou
+app.get("/den(ied|y)", advancedHandler);
+app.get("/front(-end|end)?", advancedHandler);
+
+// Expressão Regular
+app.get(/(server|client)\-?side/, advancedHandler);
+
+app.get(/(cep|postalcode)\/\d{5}\-?\d{3}/, advancedHandler);
 
 app.listen(env.PORT, console.log(`Server running at port ${env.PORT}`));
---
+++ roteamento-avancado/doc.md
@@ -0,0 +1,28 @@
+# Expressões Regulares
+
+^     INICIO DA LINHA
+$     FINAL DA LINHA
+[]    LISTA PERMITIDA
+[i-f] LISTA INTERVALADA
+\char ESCAPE
+()    GRUPO
+|     OU
+?     OPCIONAL
+{n}        QUANTIFICADOR
+{min,max}  QUANTIFICADOR INTERVALADO
++     REPETIDOR
+
+\s    ESPACO
+\d    APENAS NUMEROS
+\w    ALFANUMERICO
+
+
+# Exemplos
+
+CEP V1: ^[0-9][0-9][0-9][0-9][0-9](\s|\-)?[0-9][0-9][0-9]$
+CEP V2: ^[0-9]{5}(\-|\s)?[0-9]{3}$
+CEP V3: ^\d{5}(\-|\s)?\d{3}$
+
+EMAIL V1: ^[A-z0-9\.\-]{1,}\@(\w{2,}\.)+[A-z]{2,}$
+
+
